764260293b493c1dda0bf92c98c81ae504b54f26
MicahSee <exploretheworldofdell@gmail.com>
2017-06-22 17:28:46 -0500

    error in tasks

--- tasks.py
+++ tasks.py
@@ -4,7 +4,7 @@ import os
 broker_url = os.environ['REDIS_URL']
 
 
-celery_app = Celery(app.name, broker=broker_url, backend=broker_url )
+celery_app = Celery('tasks', broker=broker_url, backend=broker_url )
 
 celery_app.config['task_serializer'] = 'pickle'
 celery_app.config['accept_content'] = ['pickle']
